a470541afe6b15a5471b7f2083f2e416e991aba5
yimingyoupin <90834774+yimingyoupin@users.noreply.github.com>
2022-03-30 18:21:37 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "53f7850c-aa39-4470-80bf-fcd3d3e44cc1",
                         "alterId": 0
                     }
                 ],
